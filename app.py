beab0b687b458664f808e0133994c57adf32e5bd
Quentin Wach <quentin.wach@googlemail.com>
2025-12-31 16:17:12 +0100

    Fix abort import.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_file, Response
+from flask import Flask, render_template, request, jsonify, send_file, Response, abort
 import os
 import random
 import itertools
